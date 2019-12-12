a86e08deebcbe8066d55baaa23f61c4aac6343a1
blkFrank <francesco.paonessa96@gmail.com>
2019-12-12 17:29:54 +0100

    add servlet

---
+++ SoundSIW/src/servlet/Login.java
@@ -0,0 +1,67 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import objectDAO.UtenteDAO;
+import object.Utente;
+import database.DAOFactory;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@SuppressWarnings("serial")
+
+/* linkare la servlet in maniera diversa */
+
+@WebServlet("/login")
+public class Login extends HttpServlet {
+	
+
+	
+ @Override
+protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+	HttpSession session = req.getSession();
+	String paramEmail= req.getParameter("email");
+	String paramPassword = req.getParameter("password");
+	
+	
+	
+	session.setAttribute("loggato", false);
+
+	
+	System.out.println(paramEmail+" "+paramPassword);
+	DAOFactory factory = DAOFactory.getDAOFactory(DAOFactory.POSTGRESQL);
+	UtenteDAO utenteDao = factory.getUtenteDAO();
+	Utente utente = utenteDao.findByCredential(paramEmail, paramPassword);
+	
+	
+	if(utente!=null && utente.isRegistrato()) { 
+		session.setAttribute("password", paramPassword);
+		session.setAttribute("email", utente.getEmail());
+		session.setAttribute("loggato", true);
+		session.setAttribute("username", utente.getUsername());
+		
+		RequestDispatcher disp;
+		
+		
+		disp = req.getRequestDispatcher("index.jsp");
+		disp.forward(req, resp);
+		
+		}else {
+		
+		resp.sendRedirect("login.jsp");
+		
+	}
+	
+	
+}
+
+}
---
+++ SoundSIW/src/servlet/Registrazione.java
@@ -0,0 +1,57 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import objectDAO.UtenteDAO;
+import object.Utente;
+import database.DAOFactory;
+
+
+/**
+ * Servlet implementation class RegistrazioneServlet
+ */
+@SuppressWarnings("serial")
+@WebServlet("/Registrazione")
+public class Registrazione extends HttpServlet {
+	
+	 @Override
+	 protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	 	String paramUsername = req.getParameter("username");
+	 	String paramPassword = req.getParameter("password");
+	 	String paramEmail= req.getParameter("email");
+	 	String paramGenere= req.getParameter("genere");
+	 	
+	 	
+	 	if(paramEmail=="" || paramUsername=="" || paramPassword=="" ) {
+	 		resp.sendRedirect("registrazione.jsp");
+	 	}
+	 		
+	 	
+	 else {
+	 	DAOFactory factory = DAOFactory.getDAOFactory(DAOFactory.POSTGRESQL);
+	 	UtenteDAO utenteDao = factory.getUtenteDAO();
+	 	Utente utente = utenteDao.findByUsername(paramUsername);
+	 	
+	 
+	 	if(utente.getUsername().equals("")) {
+	 	
+	 		
+	 		Utente nuovoUtente=new Utente(paramUsername,paramPassword,paramEmail,paramGenere);
+	 		utenteDao.save(nuovoUtente);
+	 		System.out.println("Registrato");
+	 		
+	 		
+	 		resp.sendRedirect("login.jsp");
+	 	}else {
+	 		resp.sendRedirect("failRegister.jsp");
+	 	}
+	 }
+	 	
+	 }
+
+}
